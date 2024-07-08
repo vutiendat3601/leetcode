26c0f8053e5fce00c257429eabdc917052ce45a2
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-07-08 23:29:39 +0700

    feat: Add problem with 70. Climbing Stairs

--- Leetcode00070.java
+++ Leetcode00070.java
@@ -24,3 +24,40 @@ public class Leetcode00070 {
     System.out.println("result: " + result);
   }
 }
+
+/**
+ * You are climbing a staircase. It takes n steps to reach the top. Each time you can either climb 1
+ * or 2 steps. In how many distinct ways can you climb to the top at most k jumps?
+ */
+class Leetcode00070Version2 {
+
+  public int climbStairs(int n, int k) {
+    final int[][] dp = new int[n + 1][k + 1];
+    for (int i = 0; i <= k; i++) {
+      dp[0][i] = 1;
+      dp[1][i] = 1;
+    }
+    dp[1][0] = 0;
+
+    for (int i = 2; i <= n; i++) {
+      for (int j = 1; j <= k; j++) {
+        dp[i][j] = dp[i - 1][j - 1] + dp[i - 2][j - 1];
+      }
+    }
+    int result = 0;
+    for (int i = 0; i <= k; i++) {
+      result += dp[n][i];
+    }
+
+    return result;
+  }
+
+  /* public static void main(String[] args) {
+    final int n = 5;
+    final int k = 2;
+
+    final Leetcode00070_ test = new Leetcode00070_();
+    final int result = test.climbStairs(n, k);
+    System.out.println("result: " + result);
+  } */
+}
